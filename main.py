13b1b243f5f544f2979e53c560948edb70c9c6fb
thomascbott <thomascbott@gmail.com>
2026-07-14 20:37:59 -0400

    finish organized subdirectories dictionary

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ def main():
     # Create dictionary for organizing subdirectories and assigning file types
     ORGANIZED_SUBDIRECTORIES = {
         "Images" : [".jpg", ".jpeg", ".png", ".gif", ".webp"],
-        "Documents" : []
+        "Documents" : [".pdf", ".txt", ".docx", ".xlsx", ".csv", ".pptx"],
+        "Installers" : [".exe", ".msi", ".inf", ".appx", ".msix"],
+        "Audio" : [".mp3", ".aac", ".ogg", ".oga", ".flac", ".wav"],
+        "Video" : [".mp4", ".mkv", ".webm", ".avi", ".mpg"],
+        "Miscellaneous" : []
     }
+
+    # Iterate over the subdirectories, check if each exists in main downloads folder, if not create it
+    for subdirectory in ORGANIZED_SUBDIRECTORIES:
+
+
 
     print(downloads_directory)
 
